764d875061787c51975470cd4d8712f6f72365ae
sabualha <sabualha@andrew.cmu.edu>
2025-09-07 06:47:55 +0000

    fixed coverage for category.js

--- src/posts/category.js
+++ src/posts/category.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 const _ = require('lodash');
 
 const db = require('../database');
@@ -10,47 +9,39 @@ const activitypub = require('../activitypub');
 module.exports = function (Posts) {
 	Posts.getCidByPid = async function (pid) {
 		const tid = await Posts.getPostField(pid, 'tid');
-		console.log('SHAHD_TEST getCidByPid', pid, tid);
-		let result;
+
 		if (!tid && activitypub.helpers.isUri(pid)) {
-			result = -1; // fediverse pseudo-category
-		} else {
-			result = await topics.getTopicField(tid, 'cid');
+			return -1; // fediverse pseudo-category
 		}
-		
-		return result;
+		return topics.getTopicField(tid, 'cid');
 	};
 
 	Posts.getCidsByPids = async function (pids) {
 		const postData = await Posts.getPostsFields(pids, ['tid']);
-		const tids = _.uniq(postData.map(post => post && post.tid).filter(Boolean));
+		const tids = _.uniq(postData.map(p => p && p.tid).filter(Boolean));
 		const topicData = await topics.getTopicsFields(tids, ['cid']);
 		const tidToTopic = _.zipObject(tids, topicData);
 
-		const cids = postData.map(post => tidToTopic[post.tid] && tidToTopic[post.tid].cid);
-		return cids;
+		// guard against null posts
+		return postData.map(p => (p && tidToTopic[p.tid] && tidToTopic[p.tid].cid) || undefined);
 	};
-	
+
 	Posts.filterPidsByCid = async function (pids, cid) {
-		console.log('SHAHD_TEST filterPidsByCid', cid);
-		let filteredPids;
-		if (!cid) {
-			filteredPids = pids;
-		} else if (!Array.isArray(cid) || cid.length === 1) {
-			filteredPids = await filterPidsBySingleCid(pids, cid);
-		} else {
-			const pidsArr = await Promise.all(cid.map(c => Posts.filterPidsByCid(pids, c)));
-			filteredPids = _.union(...pidsArr);
+
+		if (!cid) return pids;
+
+		if (!Array.isArray(cid) || cid.length === 1) {
+			return filterPidsBySingleCid(pids, cid);
 		}
-		
-		return filteredPids;
+
+		const pidsArr = await Promise.all(cid.map(c => Posts.filterPidsByCid(pids, c)));
+		return _.union(...pidsArr);
 	};
-	
-	async function filterPidsBySingleCid(pids) {
-		const isMembers = await db.isSortedSetMembers('cid:${parseInt(cid, 10)}:pids', pids);
-		const result = pids.filter((pid, index) => pid && isMembers[index]);
-		return result;
+
+	async function filterPidsBySingleCid(pids, cid) {
+		const cidNum = parseInt(Array.isArray(cid) ? cid[0] : cid, 10);
+		const key = `cid:${cidNum}:pids`;
+		const isMembers = await db.isSortedSetMembers(key, pids);
+		return pids.filter((pid, index) => pid && isMembers[index]);
 	}
-
-
 };
